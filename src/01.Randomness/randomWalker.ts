3a8352604c3bae5d58eb960157d442dd8b611b9e
이예슬 <lys0119@echomarketing.co.kr>
2025-01-06 19:05:31 +0900

    작성듕...

---
+++ src/01.Randomness/randomWalker.ts
@@ -0,0 +1,80 @@
+import CanvasApp from "../common/CanvasApp";
+
+class Walker {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    this.x = width / 2;
+    this.y = height / 2;
+  }
+
+  step() {
+    const choice = Math.floor(Math.random() * 4);
+    switch (choice) {
+      case 0:
+        this.x++;
+        break;
+      case 1:
+        this.x--;
+        break;
+      case 2:
+        this.y++;
+        break;
+      case 3:
+        this.y--;
+        break;
+    }
+    // viewport 벗어나지 않도록 제한
+    this.x = Math.max(0, Math.min(this.width - 1, this.x));
+    this.y = Math.max(0, Math.min(this.height - 1, this.y));
+  }
+
+  biasedStep() {
+    const choice = Math.random();
+
+    // 40%  x+, y+
+    // 10%  x-, y-
+    if (choice < 0.4) {
+      this.x++;
+    } else if (choice < 0.8) {
+      this.y++;
+    } else if (choice < 0.9) {
+      this.x--;
+    } else {
+      this.y--;
+    }
+
+    this.x = Math.max(0, Math.min(this.width - 1, this.x));
+    this.y = Math.max(0, Math.min(this.height - 1, this.y));
+  }
+
+  show(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = "pink";
+    ctx.fillRect(this.x, this.y, 1, 1);
+  }
+}
+
+class RandomWalkerApp extends CanvasApp {
+  walker: Walker;
+
+  constructor(canvasId: string) {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    super(canvasId, width, height);
+    super(canvasId, width, height);
+    this.walker = new Walker(this.width, this.height);
+  }
+
+  animate() {
+    this.walker.step();
+    this.walker.show(this.ctx);
+  }
+}
+
+const app = new RandomWalkerApp("canvas");
+// app.start(() => app.animate());
